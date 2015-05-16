f2c30ff3c23358a308ced008df4f7cff07b35ebd
npavlidis <nikolaos.pavlidis@gmail.com>
2015-05-16 20:30:21 +0100

    latest updates

--- firewall.sh
+++ firewall.sh
@@ -17,6 +17,9 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 # Allow local programs that use loopback (Unix sockets)
 iptables -A INPUT -s 127.0.0.0/8 -d 127.0.0.0/8 -i lo -j ACCEPT
 
+# FTP-data connections allow
+iptables -A INPUT -p tcp --sport 20 --dport 1024:65535 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
 # Allow incoming traffic on defined ports
 iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
 #iptables -A INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
