666c2e2b727464d6655896879957b9e549b349cd
npavlidis <nikolaos.pavlidis@gmail.com>
2015-05-26 12:40:19 +0100

    add nikto sslscan and more

--- enumerator.sh
+++ enumerator.sh
@@ -2,6 +2,9 @@
 # Built to run on Kali Linux
 # need to run as root
 
+# TODO
+# implement -oA and maybe wafw00f
+
 [ $# -ne 2 ] && { echo "Usage: $0 output-path target-range"; exit 1; }
 
 OUTPATH=$1
@@ -37,8 +40,8 @@ for ip in $(cat $MASTERTARGETLIST); do
 	total=`wc -l $MASTERTARGETLIST | awk '{print $1}'`
 	echo -n -e "$i / $total systems scanned..." \\r
 	mkdir -p $OUTPATH/$ip
-	nmap -sS -sV -Pn --top-ports 2000 --open $ip -oN $OUTPATH/$ip/nmap-top2000tcp.txt > /dev/null 2>&1
-	nmap -sU -sV -Pn --top-ports 10 --open $ip -oN $OUTPATH/$ip/nmap-top10udp.txt > /dev/null 2>&1
+	nmap -sS -sV -Pn --reason -vvv -n --top-ports 2000 --open $ip -oN $OUTPATH/$ip/nmap-top2000tcp.txt > /dev/null 2>&1
+	nmap -sU -sV -Pn --reason -vvv -n --top-ports 10 --open $ip -oN $OUTPATH/$ip/nmap-top10udp.txt > /dev/null 2>&1
 	((i++))
 done
 echo ""
@@ -47,13 +50,13 @@ echo -e " [ \e[32mDONE \e[39m]"
 # SMTP Enumeration
 echo -n -e "Starting SMTP Enumeration..."
 for smtpip in $(grep ^25/tcp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2); do
-	nmap -Pn --script smtp-enum-users.nse --script smtp-commands.nse --script smtp-open-relay.nse -p 25 $smtpip -oN $OUTPATH/$smtpip/nmap-smtp25.txt > /dev/null 2>&1
+	nmap -Pn --reason -vvv -n --script smtp-enum-users.nse --script smtp-commands.nse --script smtp-open-relay.nse -p 25 $smtpip -oN $OUTPATH/$smtpip/nmap-smtp25.txt > /dev/null 2>&1
 done
 for smtpip in $(grep ^465/tcp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2); do
-        nmap -Pn --script smtp-enum-users.nse --script smtp-commands.nse --script smtp-open-relay.nse -p 465 $smtpip -oN $OUTPATH/$smtpip/nmap-smtp465.txt > /dev/null 2>&1
+        nmap -Pn --reason -vvv -n --script smtp-enum-users.nse --script smtp-commands.nse --script smtp-open-relay.nse -p 465 $smtpip -oN $OUTPATH/$smtpip/nmap-smtp465.txt > /dev/null 2>&1
 done
 for smtpip in $(grep ^587/tcp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2); do
-        nmap -Pn --script smtp-enum-users.nse --script smtp-commands.nse --script smtp-open-relay.nse -p 587 $smtpip -oN $OUTPATH/$smtpip/nmap-smtp587.txt > /dev/null 2>&1
+        nmap -Pn --reason -vvv -n --script smtp-enum-users.nse --script smtp-commands.nse --script smtp-open-relay.nse -p 587 $smtpip -oN $OUTPATH/$smtpip/nmap-smtp587.txt > /dev/null 2>&1
 done    
 echo -e " [ \e[32mDONE \e[39m]"
 
@@ -87,21 +90,21 @@ echo -e " [ \e[32mDONE \e[39m]"
 # FTP Enumeration
 echo -n -e "Starting FTP Enumeration..."
 for ftpip in $(grep ^21/tcp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2); do
-	nmap -Pn --script ftp-anon.nse --script-args ftp-anon.maxlist=-1 -p21 $ftpip -oN $OUTPATH/$ftpip/nmap-ftp.txt > /dev/null 2>&1
+	nmap -Pn --reason -vvv -n --script ftp-anon.nse --script-args ftp-anon.maxlist=-1 -p21 $ftpip -oN $OUTPATH/$ftpip/nmap-ftp.txt > /dev/null 2>&1
 done
 echo -e " [ \e[32mDONE \e[39m]"
 
 # Finger Enumeration
 echo -n -e "Starting Finger Enumeration..."
 for fingerip in $(grep ^79/tcp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2); do
-	nmap -Pn --script finger -p79 $fingerip -oN $OUTPATH/$fingerip/nmap-finger.txt > /dev/null 2>&1
+	nmap -Pn --reason -vvv -n --script finger -p79 $fingerip -oN $OUTPATH/$fingerip/nmap-finger.txt > /dev/null 2>&1
 done
 echo -e " [ \e[32mDONE \e[39m]"
 
 # NFS Enumeration
 echo -n -e "Starting NFS Enumeration..."
 for nfsip in $(grep ^111/tcp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2); do
-	nmap -Pn --script nfs-showmount --script nfs-ls -p111 $nfsip -oN $OUTPATH/$nfsip/nmap-nfs.txt > /dev/null 2>&1
+	nmap -Pn --reason -vvv -n --script nfs-showmount --script nfs-ls -p111 $nfsip -oN $OUTPATH/$nfsip/nmap-nfs.txt > /dev/null 2>&1
 done
 echo -e " [ \e[32mDONE \e[39m]"
 
@@ -110,7 +113,7 @@ echo -n -e "Starting SMB Enumeration..."
 grep ^445/tcp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2 > $SMBTARGETS
 grep ^139/tcp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2 >> $SMBTARGETS
 for smbip in $(sort $SMBTARGETS| uniq); do
-	nmap -Pn --script smb-check-vulns.nse --script-args unsafe=1 -p445,139 $smbip -oN $OUTPATH/$smbip/nmap-smb.txt > /dev/null 2>&1
+	nmap -Pn --reason -vvv -n --script smb-check-vulns.nse --script-args unsafe=1 -p445,139 $smbip -oN $OUTPATH/$smbip/nmap-smb.txt > /dev/null 2>&1
 	enum4linux -a -v > $OUTPATH/$smbip/enum4linux.txt 2>&1
 done
 echo -e " [ \e[32mDONE \e[39m]"
@@ -118,7 +121,7 @@ echo -e " [ \e[32mDONE \e[39m]"
 # TFTP Enumeration
 echo -n -e "Starting TFTP Enumeration..."
 for tftpip in $(grep ^69/udp $OUTPATH/* -R | grep -v filtered | cut -d "/" -f 2); do
-	nmap -Pn -sU -p 69 --script tftp-enum.nse $tftpip -oN $OUTPATH/$tftpip/nmap-tftp.txt > /dev/null 2>&1
+	nmap -Pn --reason -vvv -n -sU -p 69 --script tftp-enum.nse $tftpip -oN $OUTPATH/$tftpip/nmap-tftp.txt > /dev/null 2>&1
 done
 echo -e " [ \e[32mDONE \e[39m]"
 
@@ -130,7 +133,8 @@ for httpipport in $(sort $HTTPTARGETS | uniq); do
 	httpip=`echo $httpipport | cut -d ":" -f1`
 	httpport=`echo $httpipport | cut -d ":" -f2`
 	/opt/cutycapt/CutyCapt --url=http://$httpip:$httpport/ --out=$OUTPATH/$httpip/web-port-$httpport.png --out-format=png --java=on --min-width=1680 --min-height=1050
-	nmap -Pn -p $httpport --script http-headers --script http-methods --script http-title --script http-auth-finder --script http-enum $httpip -oN $OUTPATH/$httpip/web-port-$httpport-nmap-http.txt > /dev/null 2>&1
+	nmap --reason -vvv -n -Pn -p $httpport --script http-headers --script http-methods --script http-title --script http-auth-finder --script http-enum $httpip -oN $OUTPATH/$httpip/web-port-$httpport-nmap-http.txt > /dev/null 2>&1
+	nikto -nolookup -nointeractive -ask no -Format txt -output $OUTPATH/$httpip/web-port-$httpport-nikto.txt -port $httpport -host $httpip > /dev/null 2>&1
 	echo $httpipport >> $HTTPTARGETS
 done
 echo -e " [ \e[32mDONE \e[39m]"
@@ -141,6 +145,8 @@ for httpsipport in $(grep ssl $OUTPATH/*/nmap* -R | grep -v '\s http' | grep ope
 	httpsip=`echo $httpsipport | cut -d ":" -f1`
 	httpsport=`echo $httpsipport | cut -d ":" -f2`
 	/opt/cutycapt/CutyCapt --insecure --url=https://$httpsip:$httpsport/ --out=$OUTPATH/$httpsip/web-port-$httpsport.png --out-format=png --java=on --min-width=1680 --min-height=1050 
+	nikto -nolookup -nointeractive -ask no -ssl -Format txt -output $OUTPATH/$httpsip/web-port-$httpsport-nikto.txt -port $httpsport -host $httpsip > /dev/null 2>&1
+	sslscan --show-certificate --no-colour $httpsip:$httpsport > $OUTPATH/$httpsip/sslscan-$httpsport.txt 2>&1
 done
 echo -e " [ \e[32mDONE \e[39m]"
 # Thats all folks!
